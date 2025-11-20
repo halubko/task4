38a8ce7cb32279d565e1b2f036c5714bf4c395c5
halubko <misha.golubko@gmail.com>
2025-11-21 01:52:07 +0300

    styles changes

--- src/assets/styles/theme.ts
+++ src/assets/styles/theme.ts
@@ -1,12 +1,25 @@
 export const theme = {
    colors: {
-      main_bg: "#141414",
-      content_bg: "#222222",
+      background: {
+         main: "#141414",
+         content: "#222222",
+         input: "#2c2c2c",
+         error: "#f36860",
+         primary_button: "#e1e3e6",
+         primary_button_hover: "rgba(225,227,230, 0.7)",
+         secondary_button: "#222222",
+         secondary_button_hover: "rgba(255, 255, 255, 0.2)",
+      },
       text: {
          primary: "#e1e3e6",
          secondary: "#e1e3e6",
          link: "#71aaeb",
          link_selected: "#bdd5e8",
+         error: "#000000",
       },
+   },
+   borders: {
+      base: "1px solid rgba(255, 255, 255, 0.2)",
+      focus: "1px solid #71aaeb",
    },
 };
--- src/types/emotion.d.ts
+++ src/types/emotion.d.ts
@@ -1,18 +1,33 @@
 import "@emotion/react";
 
 interface ThemeColors {
-   main_bg: string;
-   content_bg: string;
+   background: {
+      main: string;
+      content: string;
+      input: string;
+      error: string;
+      primary_button: string;
+      secondary_button: string;
+      primary_button_hover: string;
+      secondary_button_hover: string;
+   };
    text: {
       primary: string;
       secondary: string;
       link: string;
       link_selected: string;
+      error: string;
    };
+}
+
+interface ThemeBorders {
+   base: string;
+   focus: string;
 }
 
 declare module "@emotion/react" {
    export interface Theme {
       colors: ThemeColors;
+      borders: ThemeBorders;
    }
 }
