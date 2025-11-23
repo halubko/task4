6a0a70e6126339358d43de69a4fc6795d263374d
halubko <misha.golubko@gmail.com>
2025-11-24 00:21:57 +0300

    added public api for profile

--- src/modules/profile/hooks/useGetProfile.ts
+++ src/modules/profile/hooks/useGetProfile.ts
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { getUser } from "@/modules/profile/api/api.ts";
 
-export const useGetUser = (id: number) => {
+export const useGetProfile = (id: number) => {
    return useQuery({
       queryKey: ["get", "user", id],
       queryFn: () => getUser(id),
