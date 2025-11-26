f1c3a5ccc1feb3b2b4acb3beb0a87e5b04477587
halubko <misha.golubko@gmail.com>
2025-11-26 17:31:20 +0300

    mok data added only for posts page

--- src/modules/posts/components/PostsList.tsx
+++ src/modules/posts/components/PostsList.tsx
@@ -7,6 +7,7 @@ import { mokPosts } from "@/modules/posts/data/mokPosts.ts";
 import PostModal from "@/modules/posts/components/PostModal.tsx";
 import PostCreateButton from "@/modules/posts/components/ui/PostCreateButton.tsx";
 import LoadingIndicator from "@/shared/LoadingIndicator.tsx";
+import { useLocation } from "@tanstack/react-router";
 
 const Wrapper = styled.div`
    display: flex;
@@ -17,6 +18,7 @@ const Wrapper = styled.div`
 export const PostsList = () => {
    const { data, fetchNextPage, isFetching } = useGetPosts();
    const { ref, inView } = useInView();
+   const { pathname } = useLocation();
 
    useEffect(() => {
       if (inView) {
@@ -29,9 +31,8 @@ export const PostsList = () => {
          <PostCreateButton />
          {data?.pages.map((page, i) => (
             <Wrapper key={i}>
-               {mokPosts.map((post) => (
-                  <Post key={post.id} {...post} />
-               ))}
+               {/* Mok data added only for /posts. */}
+               {pathname === "/posts" && mokPosts.map((post) => <Post key={post.id} {...post} />)}
                {page.posts.map((post) => (
                   <Post key={post.id} {...post} />
                ))}
