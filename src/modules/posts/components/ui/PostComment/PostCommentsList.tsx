3fb7265fbd1db801ca4e51495b4301509422092d
halubko <misha.golubko@gmail.com>
2025-11-26 18:31:24 +0300

    created hook useSetComment and fixed ui

--- src/modules/posts/components/ui/PostComment/PostCommentsList.tsx
+++ src/modules/posts/components/ui/PostComment/PostCommentsList.tsx
@@ -2,6 +2,7 @@ import type { PostCommentInterface } from "@/modules/posts/interfaces/Comment.In
 import PostComment from "@/modules/posts/components/ui/PostComment/PostComment.tsx";
 import styled from "@emotion/styled";
 import AddComment from "@/modules/posts/components/ui/PostComment/AddCommentForm.tsx";
+import { Fragment } from "react";
 
 const Wrapper = styled.div`
    display: flex;
@@ -11,21 +12,22 @@ const Wrapper = styled.div`
 
 interface PostCommentsProps {
    comments: PostCommentInterface[];
+   postId: number;
 }
 
-const PostCommentsList = ({ comments }: PostCommentsProps) => {
+const PostCommentsList = ({ comments, postId }: PostCommentsProps) => {
    return (
       <Wrapper>
          {comments.map((comment, index) => (
-            <>
-               <PostComment comment={comment} key={comment.id} />
+            <Fragment key={comment.id}>
+               <PostComment comment={comment} />
                {comments.length > 1 && index !== comments.length - 1 && (
                   <hr style={{ margin: "8px 0", opacity: 0.5 }} />
                )}
-            </>
+            </Fragment>
          ))}
-         <hr style={{ margin: "8px 0", opacity: 0.5 }} />
-         <AddComment postId={comments[0].postId} />
+         {comments.length > 1 && <hr style={{ margin: "8px 0", opacity: 0.5 }} />}
+         <AddComment postId={postId} />
       </Wrapper>
    );
 };
