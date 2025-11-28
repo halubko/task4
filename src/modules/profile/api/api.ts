99666bcc470332bf87ffb5122ef8a6c614c1bc26
halubko <misha.golubko@gmail.com>
2025-11-28 07:51:06 +0300

    added delete user hook

--- src/modules/profile/api/api.ts
+++ src/modules/profile/api/api.ts
@@ -10,3 +10,8 @@ export const getUser = async (id: number) => {
    const response: AxiosResponse<ProfileInfoInterface> = await axiosInstance.get(`/${id}`);
    return response.data;
 };
+
+export const deleteUser = async (id: number) => {
+   const response: AxiosResponse<ProfileInfoInterface> = await axiosInstance.delete(`/${id}`);
+   return response.data;
+};
---
+++ src/modules/profile/hooks/useDeleteUser.ts
@@ -0,0 +1,26 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteUser } from "@/modules/profile/api/api.ts";
+import { toast } from "react-toastify";
+import { authStore } from "@/modules/auth";
+import { useNavigate } from "@tanstack/react-router";
+
+const useDeleteUser = () => {
+   const navigate = useNavigate();
+   const queryClient = useQueryClient();
+
+   return useMutation({
+      mutationKey: ["delete", "user"],
+      mutationFn: deleteUser,
+      onSuccess: () => {
+         toast.success("Account deleted!");
+         authStore.logout();
+         queryClient.clear();
+         navigate({ to: "/signin" });
+      },
+      onError: (error) => {
+         toast.error("Error while deleting account: " + error.message);
+      },
+   });
+};
+
+export default useDeleteUser;
