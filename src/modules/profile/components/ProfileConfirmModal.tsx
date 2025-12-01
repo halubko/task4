cafc7378098bb4f8c359361238edb08c81e505f2
halubko <misha.golubko@gmail.com>
2025-12-01 20:36:24 +0300

    fixed modal ui

--- src/modules/profile/components/ProfileConfirmModal.tsx
+++ src/modules/profile/components/ProfileConfirmModal.tsx
@@ -1,7 +1,6 @@
 import useDeleteUser from "@/modules/profile/hooks/useDeleteUser.ts";
 import { authStore } from "@/modules/auth";
 import styled from "@emotion/styled";
-import BaseButton from "@/shared/Base/BaseButton.tsx";
 import ModalTemplate from "@/shared/ModalTemplate.tsx";
 import ProfileStore from "@/modules/profile/store/profile.store.ts";
 import { observer } from "mobx-react-lite";
@@ -12,6 +11,24 @@ const Wrapper = styled.div`
    background-color: ${({ theme }) => theme.colors.background.content};
    border: ${({ theme }) => theme.borders.base};
    border-radius: 8px;
+   padding: 20px;
+   gap: 42px;
+   justify-self: center;
+`;
+
+const Button = styled.button<{ variant?: "delete" }>`
+   padding: 8px;
+   background: transparent;
+   cursor: pointer;
+   font-size: 1.5rem;
+   font-weight: 600;
+   color: ${({ variant, theme }) => (variant === "delete" ? "#e02424" : theme.colors.text.primary)};
+   transition: background-color 0.2s;
+   border: ${({ theme }) => theme.borders.base};
+   border-radius: 8px;
+   &:hover {
+      background-color: #333;
+   }
 `;
 
 export const ProfileConfirmModal = observer(() => {
@@ -22,14 +39,14 @@ export const ProfileConfirmModal = observer(() => {
    return (
       <ModalTemplate>
          <Wrapper>
-            Are you sure you want to delete this profile?
+            <h2 style={{ textAlign: "center", fontSize: "1.7rem" }}>
+               Are you sure you want to delete your profile?
+            </h2>
             <div style={{ display: "flex", justifyContent: "space-between" }}>
-               <BaseButton type="secondary" onClick={() => ProfileStore.setConfirmModalOpen(false)}>
-                  Close
-               </BaseButton>
-               <BaseButton type="primary" onClick={() => mutate(authStore.id)}>
+               <Button onClick={() => ProfileStore.setConfirmModalOpen(false)}>Close</Button>
+               <Button variant="delete" onClick={() => mutate(authStore.id)}>
                   Delete
-               </BaseButton>
+               </Button>
             </div>
          </Wrapper>
       </ModalTemplate>
