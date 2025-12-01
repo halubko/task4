f0c223e3f2241312c96070c9d4bbdeb7b85f9eb2
halubko <misha.golubko@gmail.com>
2025-12-01 21:04:06 +0300

    updating get query after editing

--- src/modules/profile/hooks/useUpdateUser.ts
+++ src/modules/profile/hooks/useUpdateUser.ts
@@ -1,4 +1,4 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { updateUser } from "@/modules/profile/api/api.ts";
 import { useNavigate } from "@tanstack/react-router";
 import { toast } from "react-toastify";
@@ -9,12 +9,14 @@ import type {
 
 const useUpdateUser = (userId: number) => {
    const navigate = useNavigate();
+   const { invalidateQueries } = useQueryClient();
 
    return useMutation<ProfileInfoInterface, Error, ProfileUpdateInterface>({
       mutationKey: ["update", "user"],
       mutationFn: (formData) => updateUser(userId, formData),
       onSuccess: () => {
          toast.success("Profile updated successfully.");
+         invalidateQueries({ queryKey: ["get", "user", userId] });
          navigate({ to: `/profile/${userId}` });
       },
       onError: (error) => {
