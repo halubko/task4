af02ee380ff0f3a937f43ef59db939d0fd25bb8d
halubko <misha.golubko@gmail.com>
2025-12-07 00:24:39 +0300

    ui reworked

--- src/modules/chat/components/Chat.tsx
+++ src/modules/chat/components/Chat.tsx
@@ -1,34 +1,34 @@
 import styled from "@emotion/styled";
 import ChatHeader from "@/modules/chat/components/ui/ChatHeader/ChatHeader.tsx";
-import { Route } from "@/routes/_main/messages/$userId.ts";
 import { useGetProfile } from "@/modules/profile";
 import ChatFooter from "@/modules/chat/components/ui/ChatFooter/ChatFooter.tsx";
 import ChatMain from "@/modules/chat/components/ui/ChatMain/ChatMain.tsx";
+import { useParams } from "@tanstack/react-router";
 
-const Wrapper = styled.div`
+export const Wrapper = styled.div`
    background-color: ${({ theme }) => theme.colors.background.content};
    border: ${({ theme }) => theme.borders.base};
    border-radius: 8px;
-   max-width: 550px;
-   margin: 0 auto;
-   height: 90vh;
    display: flex;
    flex-direction: column;
+   height: 100%;
 `;
 
 export const Chat = () => {
-   const { userId } = Route.useParams();
+   const { userId } = useParams({ strict: false });
    const profileId = Number(userId);
    const { data } = useGetProfile(profileId);
 
    return (
       <>
          {data && (
-            <Wrapper>
-               <ChatHeader userId={profileId} profileName={data.firstName} />
-               <ChatMain profileId={profileId} />
-               <ChatFooter />
-            </Wrapper>
+            <div style={{ width: "100%" }}>
+               <Wrapper>
+                  <ChatHeader userId={profileId} profileName={data.firstName} />
+                  <ChatMain profileId={profileId} />
+                  <ChatFooter />
+               </Wrapper>
+            </div>
          )}
       </>
    );
