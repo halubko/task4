445c64a38fcb6fee3663a0c00991de21776efb94
halubko <misha.golubko@gmail.com>
2025-12-07 00:13:14 +0300

    added mok data for chat

---
+++ src/modules/chat/data/mokChatsList.ts
@@ -0,0 +1,22 @@
+import type { ChatInterface } from "@/modules/chat/interfaces/Chat.Interface.ts";
+
+export const mokChatsList: ChatInterface[] = [
+   {
+      id: crypto.randomUUID(),
+      recipientId: 2,
+      last_sender: "You",
+      last_message: "Hello",
+   },
+   {
+      id: crypto.randomUUID(),
+      recipientId: 5,
+      last_sender: "Emma",
+      last_message: "Have a good day!",
+   },
+   {
+      id: crypto.randomUUID(),
+      recipientId: 10,
+      last_sender: "Isabella",
+      last_message: "Ok",
+   },
+];
